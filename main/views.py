f84a6112ac9773a8607428612c579fddc0059ed5
AnaghaARao <rao.anagha2002@gmail.com>
2024-05-02 18:49:56 +0530

    Updated API call

--- main/views.py
+++ main/views.py
@@ -14,13 +14,13 @@ load_dotenv()
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        
+        state = request.POST['state']
+        country = request.POST['country']
         # getting api key
         api_key = os.getenv("API_KEY")
         # source contain JSON data from API
 
-        source = urllib.request.urlopen(
-             http://api.openweathermap.org/geo/1.0/direct?q={city},{state code},{country code}&limit={limit}&appid={api_key}).read()
+        source = urllib.request.urlopen('http://api.openweathermap.org/geo/1.0/direct?q='+ city + ',' + state + ',' + country + '&limit=5&appid=' + api_key).read()
         
         # converting JSON data to a dictionary
         list_of_data = json.loads(source)
